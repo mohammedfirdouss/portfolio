9c4fee0d49c9ee8bfbbef31c3e2e8ad93486e26c
Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>
2025-07-15 09:17:02 +0000

    refactor: enhance styling for DiagramArticle and OpenSourceArticle components

--- app/diagrams/article.tsx
+++ app/diagrams/article.tsx
@@ -12,7 +12,7 @@ export function DiagramArticle({ entry, views }: Props) {
     <Link href={`/diagrams/${entry.slug}`}>
       <article className="p-4 md:p-8">
         <div className="flex justify-between gap-2 items-center">
-          <span className="text-xs text-zinc-200">
+          <span className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
             {entry.date ? (
               <time dateTime={new Date(entry.date).toISOString()}>
                 {Intl.DateTimeFormat(undefined, { dateStyle: "medium" }).format(
@@ -27,11 +27,11 @@ export function DiagramArticle({ entry, views }: Props) {
             <Eye className="w-4 h-4" /> {Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
           </span>
         </div>
-        <h2 className="text-xl font-medium lg:text-3xl text-zinc-200">
+        <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
           {entry.title}
         </h2>
-        <p className="mt-4 text-sm text-zinc-400">
-          {entry.summary}
+        <p className="z-20 mt-4 text-sm duration-1000 text-zinc-400 group-hover:text-zinc-200">
+          {entry.summary || "No summary available."}
         </p>
       </article>
     </Link>
--- app/open-source/article.tsx
+++ app/open-source/article.tsx
@@ -12,7 +12,7 @@ export function OpenSourceArticle({ entry, views }: Props) {
     <Link href={`/open-source/${entry.slug}`}>
       <article className="p-4 md:p-8">
         <div className="flex justify-between gap-2 items-center">
-          <span className="text-xs text-zinc-200">
+          <span className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
             {entry.date ? (
               <time dateTime={new Date(entry.date).toISOString()}>
                 {Intl.DateTimeFormat(undefined, { dateStyle: "medium" }).format(
@@ -24,13 +24,14 @@ export function OpenSourceArticle({ entry, views }: Props) {
             )}
           </span>
           <span className="text-zinc-500 text-xs flex items-center gap-1">
-            <Eye className="w-4 h-4" /> {Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
+            <Eye className="w-4 h-4" />{" "}
+            {Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
           </span>
         </div>
-        <h2 className="text-xl font-medium lg:text-3xl text-zinc-200">
+        <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
           {entry.title}
         </h2>
-        <p className="mt-4 text-sm text-zinc-400">
+        <p className="z-20 mt-4 text-sm duration-1000 text-zinc-400 group-hover:text-zinc-200">
           {entry.summary || "No summary available."}
         </p>
       </article>
