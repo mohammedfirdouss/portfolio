649c3299b6425735754be4236ae6c23ae28740bd
mohammedfirdouss <mohammedfirdousaraoye@gmail.com>
2025-09-04 11:24:17 +0000

    changes

--- app/open-source/layout.tsx
+++ app/open-source/layout.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import { Metadata } from "next";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Open Source | Portfolio",
