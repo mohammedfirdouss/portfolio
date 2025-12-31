9e05cf0e54cd7b90618d678095d30c4f08b0f982
Mohammed Firdous <mohammedfirdousaraoye@gmail.com>
2025-12-31 15:48:37 +0000

    add changelog page and styles; update navigation links

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ const navigation = [
   { name: "Experience", href: "/experience" },
   { name: "Projects", href: "/projects" },
   { name: "Blog", href: "/blog" },
+  { name: "Changelog", href: "/changelog" },
   { name: "Open Source", href: "/open-source" },
   { name: "Diagrams", href: "/diagrams" },
   { name: "Contact", href: "/contact" },
