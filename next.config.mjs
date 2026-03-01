ef0184fd5f76c2efdd8de27174ccd505a511f706
Mohammed Firdous <mohammedfirdousaraoye@gmail.com>
2026-03-01 08:24:42 +0000

    refactor: set output configuration to standalone in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const bundleAnalyzer = withBundleAnalyzer({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: "standalone",
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 	experimental: {
 		mdxRs: true,
